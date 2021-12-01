736b93ce214d0885cf8f7c44b7af6c13416a1859
Rickard Andersson <dunklas@gmail.com>
2021-12-01 06:32:54 +0100

    Set up project

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace aoc_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Error.WriteLine("No day provided");
+                Environment.Exit(1);
+            }
+            var day = args[0];
+
+            switch (day)
+            {
+                case "1":
+                    Console.WriteLine("Yay");
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
