d08f827a33219a89e6f1fdf915226356c70e1c69
Rickard Andersson <dunklas@gmail.com>
2021-12-01 20:18:39 +0100

    Remove namespace block

--- src/solutions/Day1.cs
+++ src/solutions/Day1.cs
@@ -2,47 +2,45 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace aoc_2021.solutions
+namespace aoc_2021.solutions;
+public class Day1
 {
-    public class Day1
+    public void Solve(string input)
     {
-        public void Solve(string input)
-        {
-            Console.WriteLine(SolvePart1(input));
-            Console.WriteLine(SolvePart2(input));
-        }
+        Console.WriteLine(SolvePart1(input));
+        Console.WriteLine(SolvePart2(input));
+    }
 
-        public int SolvePart1(string input)
-        {
-            var depthMeasurements = ParseInput(input);
-            var totalIncreases = Enumerable.Range(0, depthMeasurements.Count - 1)
-                .Where(x => depthMeasurements[x + 1] > depthMeasurements[x])
-                .Count();
-            return totalIncreases;
-        }
+    public int SolvePart1(string input)
+    {
+        var depthMeasurements = ParseInput(input);
+        var totalIncreases = Enumerable.Range(0, depthMeasurements.Count - 1)
+            .Where(x => depthMeasurements[x + 1] > depthMeasurements[x])
+            .Count();
+        return totalIncreases;
+    }
 
-        public int SolvePart2(string input)
-        {
-            var depthMeasurements = ParseInput(input);
-            var measurementWindows = MeasurementWindows(depthMeasurements, 3);
-            var totalIncreases = Enumerable.Range(0, measurementWindows.Count - 1)
-                .Where(x => measurementWindows[x + 1] > measurementWindows[x])
-                .Count();
-            return totalIncreases;
-        }
+    public int SolvePart2(string input)
+    {
+        var depthMeasurements = ParseInput(input);
+        var measurementWindows = MeasurementWindows(depthMeasurements, 3);
+        var totalIncreases = Enumerable.Range(0, measurementWindows.Count - 1)
+            .Where(x => measurementWindows[x + 1] > measurementWindows[x])
+            .Count();
+        return totalIncreases;
+    }
 
-        private List<int> MeasurementWindows(List<int> measurements, int windowSize)
-        {
-            return Enumerable.Range(0, measurements.Count - windowSize + 1)
-                .Select(x => measurements.GetRange(x, windowSize).Sum())
-                .ToList();
-        }
+    private List<int> MeasurementWindows(List<int> measurements, int windowSize)
+    {
+        return Enumerable.Range(0, measurements.Count - windowSize + 1)
+            .Select(x => measurements.GetRange(x, windowSize).Sum())
+            .ToList();
+    }
 
-        private List<int> ParseInput(string input)
-        {
-            return input.Split("\n")
-                .Select(line => int.Parse(line))
-                .ToList();
-        }
+    private List<int> ParseInput(string input)
+    {
+        return input.Split("\n")
+            .Select(line => int.Parse(line))
+            .ToList();
     }
 }
