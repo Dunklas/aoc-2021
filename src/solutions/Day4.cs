0e406a9c7d5e56ddb056e534ba1be13fa7b8ba71
Rickard Andersson <dunklas@gmail.com>
2021-12-04 11:34:51 +0100

    Introduce board class

--- src/solutions/Day4.cs
+++ src/solutions/Day4.cs
@@ -5,8 +5,6 @@ using System.Text.RegularExpressions;
 
 namespace aoc_2021.solutions;
 
-public record BingoInput(List<int> Numbers, List<HashSet<List<int>>> Boards);
-
 public class Day4
 {
     public void Solve(string input)
@@ -18,19 +16,17 @@ public class Day4
     public int SolvePart1(string input)
     {
         var parsed = ParseInput(input);
-        for (int i = 4; i < parsed.Numbers.Count(); i++)
+        for (int i = 4; i < parsed.Item1.Count(); i++)
         {
-            var drawedNumbers = parsed.Numbers.GetRange(0, i + 1).ToHashSet();
-            var winner = parsed.Boards
+            var drawedNumbers = parsed.Item1.GetRange(0, i + 1).ToHashSet();
+            var winner = parsed.Item2
                 .Where(board =>
-                    board.Any(row =>
-                        row.ToHashSet().IsSubsetOf(drawedNumbers)
-                    )
+                    board.AllRows.Any(row => row.IsSubsetOf(drawedNumbers))
                 )
                 .FirstOrDefault();
             if (winner != null)
             {
-                var allNumbers = winner
+                var allNumbers = winner.AllRows
                     .SelectMany(row => row)
                     .ToHashSet();
                 var sum = allNumbers.Except(drawedNumbers)
@@ -45,39 +41,34 @@ public class Day4
     {
         var parsed = ParseInput(input);
         HashSet<int> lastDrawedNumbers = null;
-        List<HashSet<List<int>>> winners = new List<HashSet<List<int>>>();
-        for (int i = 4; i < parsed.Numbers.Count(); i++)
+        var winners = new List<Board>();
+        for (int i = 4; i < parsed.Item1.Count(); i++)
         {
-            var drawedNumbers = parsed.Numbers.GetRange(0, i + 1).ToHashSet();
-            parsed.Boards
+            var drawedNumbers = parsed.Item1.GetRange(0, i + 1).ToHashSet();
+            parsed.Item2
                 .Where(board =>
-                    board.Any(row =>
-                        row.ToHashSet().IsSubsetOf(drawedNumbers)
-                    )
-                )
+                    board.AllRows.Any(row => row.IsSubsetOf(drawedNumbers)
+                ))
                 .ToList()
                 .ForEach(winner =>
                 {
                     if (!winners.Contains(winner))
                     {
-                        Console.WriteLine("Board won");
                         winners.Add(winner);
                         lastDrawedNumbers = drawedNumbers;
                     }
                 });
         }
-        Console.WriteLine(winners.Count());
         var lastWinner = winners.Last();
-        var allNumbers = lastWinner
+        var allNumbers = lastWinner.AllRows
             .SelectMany(row => row)
             .ToHashSet();
         var sum = allNumbers.Except(lastDrawedNumbers)
             .Sum();
-        Console.WriteLine("Sum " + sum);
         return sum * lastDrawedNumbers.Last();
     }
 
-    private BingoInput ParseInput(string input)
+    private (List<int>, List<Board>) ParseInput(string input)
     {
         var parts = input.Split("\n\n");
         var numbers = parts[0].Split(",")
@@ -87,28 +78,40 @@ public class Day4
         var boards = Enumerable.Range(1, parts.Count() - 1)
             .Select(i =>
             {
-                var allRows = new HashSet<List<int>>();
-                var allColumns = new HashSet<List<int>>();
-                var rows = parts[i].Split("\n");
+                var rows = new HashSet<List<int>>();
+                var columns = new HashSet<List<int>>();
+                var rawRows = parts[i].Split("\n");
                 for (int j = 0; j < 5; j++)
                 {
-                    var rowNumbers = Regex.Split(rows[j].Trim(), " +");
+                    var rowNumbers = Regex.Split(rawRows[j].Trim(), " +");
                     var row = new List<int>();
                     for (int k = 0; k < 5; k++)
                     {
                         row.Add(int.Parse(rowNumbers[k]));
                     }
-                    allRows.Add(row);
+                    rows.Add(row);
                 }
                 for (int j = 0; j < 5; j++)
                 {
-                    allColumns.Add(allRows.ToList()
+                    columns.Add(rows.ToList()
                         .Select(row => row[j])
                         .ToList());
                 }
-                return allRows.Union(allColumns).ToHashSet();
+                return new Board(rows, columns);
             })
             .ToList();
-        return new BingoInput(numbers, boards);
+        return (numbers, boards);
+    }
+}
+
+public class Board
+{
+    public List<HashSet<int>> AllRows { get; }
+
+    public Board(HashSet<List<int>> rows, HashSet<List<int>> columns)
+    {
+        AllRows = rows.Union(columns)
+            .Select(row => row.ToHashSet())
+            .ToList();
     }
 }
